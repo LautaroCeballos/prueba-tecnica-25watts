9f7c99f05f403eed7d35ca99d5ead9d3651c31d1
Lautaro <lautaroceballos2013@gmail.com>
2021-06-13 08:35:01 -0300

    About responsive

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -15,29 +15,31 @@ export default function About(){
         { src: friendsIcon, title: 'Lorem ipsum dolor sit amet' }
     ]    
 
-    return <section className="about">
-        <ul className="grid">{
-            aboutIcons.map(icon => {
-                return (
-                    <li key={icon.src}>
-                        <img src={icon.src} alt={icon.alt}/>
-                        <h4>{icon.title}</h4>
-                    </li>
-                )
-            })            
-        }</ul>
+    return (
+        <section className="about">
+            <ul className="grid">{
+                aboutIcons.map(icon => {
+                    return (
+                        <li key={icon.src}>
+                            <img src={icon.src} alt={icon.alt}/>
+                            <h4>{icon.title}</h4>
+                        </li>
+                    )
+                })            
+            }</ul>
 
-        <article id="about">
-            <h2 className="title">
-                We Help Businesses <br/> 
-                <span className="orange-text">
-                    Grow and Innovate
-                </span>
-            </h2>
+            <article id="about">
+                <h2 className="title">
+                    We Help Businesses <br/> 
+                    <span className="orange-text">
+                        Grow and Innovate
+                    </span>
+                </h2>
 
-            <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-            </p>
-        </article>
-    </section>
+                <p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                </p>
+            </article>
+        </section>
+    )
 }
